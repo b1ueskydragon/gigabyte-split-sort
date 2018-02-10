d18041ad61b2374c23823699a451c1268bbb611f
inagon <dragoalie@gmail.com>
2018-02-11 03:36:58 +0900

    Replace while-true to while

--- src/jp/ma/jang/sort/QuickSort.java
+++ src/jp/ma/jang/sort/QuickSort.java
@@ -1,6 +1,8 @@
 
 package jp.ma.jang.sort;
 
+import java.util.Arrays;
+
 public class QuickSort {
   /**
    * 配列の要素を相互交換する
@@ -21,20 +23,19 @@ public class QuickSort {
    * @param ary   配列
    * @param left  分割する区間の先頭要素の index
    * @param right 分割する区間の末尾要素の index
-   * @return      ソート済みの配列
+   * @return ソート済みの配列
    */
   public int[] quickSort(int[] ary, int left, int right) {
     int cl = left;
     int cr = right;
     int center = ary[(cl + cr) / 2];
 
-    while (true) {
+    // カーソルの左右の向きが変わったら抜け出す
+    while (cl <= cr) {
       while (ary[cl] < center) cl++;
       while (ary[cr] > center) cr--;
-      if (cl <= cr) swap(ary, cl++, cr--);
 
-      // カーソルの左右の向きが変わったら抜け出す
-      if (cl > cr) break;
+      if (cl <= cr) swap(ary, cl++, cr--);
     }
 
     // 左半分
